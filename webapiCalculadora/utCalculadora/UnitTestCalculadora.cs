f4a2b40f41b9fb9c1a65a27017a40e2019af4749
michehl16@gmail.com <visual_community@outlook.com>
2020-03-07 11:50:07 -0400

    los 8 commit  test

--- webapiCalculadora/utCalculadora/UnitTestCalculadora.cs
+++ webapiCalculadora/utCalculadora/UnitTestCalculadora.cs
@@ -22,5 +22,115 @@ namespace utCalculadora
             //Assert =Verification
             Assert.AreEqual(numres, result);
         }
+        [TestMethod]
+        public void TestAddGet2()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = -4;
+            int numres = -7;
+
+            //Act = Ejecucion
+            int result = calc.Add(numa, numb);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMultiplyGet3()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = 3;
+            int numb = 4;
+            int numres = 12;
+
+            //Act = Ejecucion
+            int result = calc.Multiply(numa, numb);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMultiplyGet4()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = -4;
+            int numres = 12;
+
+            //Act = Ejecucion
+            int result = calc.Multiply(numa, numb);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMultiplyGet5()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = -4;
+            int numc = -2;
+            int numres = 12;
+
+            //Act = Ejecucion
+            int result = calc.Multiply(numa, numb, numc);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMultiplyGet6()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = 4;
+            int numc = -2;
+            int numres = -12;
+
+            //Act = Ejecucion
+            int result = calc.Multiply(numa, numb, numc);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestAddGet7()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = 3;
+            int numb = 4;
+            int numc = -2;
+            int numres = 7;
+
+            //Act = Ejecucion
+            int result = calc.Add(numa, numb, numc);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestAddGet8()
+        {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = 4;
+            int numc = -2;
+            int numres = 1;
+
+            //Act = Ejecucion
+            int result = calc.Add(numa, numb, numc);
+
+            //Assert =Verification
+            Assert.AreEqual(numres, result);
+        }
+
     }
 }
